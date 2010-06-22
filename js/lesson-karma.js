fc24e5c1eafdc810b34c8538ff348f806012b5c9
Peter <Peter.Gijsels@gmail.com>
2010-06-22 15:57:40 +0545

    initial version

---
+++ js/lesson-karma.js
@@ -0,0 +1,5 @@
+function lesson_karma() {
+  return Karma({
+    image: [],
+    audio: []});
+}
---
+++ js/lesson.js
@@ -0,0 +1,5 @@
+var lesson_data = {
+    objectPosition: [465, 69, 943, 115, 72, 249, 820, 330, 475, 405, 910, 425, 275, 553, 777, 581] ,
+    objectDimension: [130, 150, 140, 165, 170, 95, 130, 200] ,
+    imgNames: ['crow', 'parrot', 'eagle', 'pigeon', 'mynah', 'owl', 'egret', 'sparrow']
+};
